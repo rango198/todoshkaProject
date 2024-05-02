6861ba620f5a9b2f22d8ca8b6918ff30dc7b658b
sashasashkina <koshyl.apps@gmail.com>
2024-05-02 17:24:23 +0300

    commit1

--- src/pages/AuthPage/AuthFormPage/LoginPage.jsx
+++ src/pages/AuthPage/AuthFormPage/LoginPage.jsx
@@ -1,7 +1,10 @@
+import LoginForm from "../../../components/AuthForm/LoginForm/LoginForm";
+
 const LoginPage = () => {
   return (
     <div>
       <h2>LoginPage</h2>
+      <LoginForm />
     </div>
   );
 };
