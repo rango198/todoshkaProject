3432a81314071663609a487a110873290a631b81
sashasashkina <koshyl.apps@gmail.com>
2024-05-02 17:15:02 +0300

    registerformfirst

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
 
-import WelcomePage from "./pages/WelcomePage/WelcomePage";
-import HomePage from "./pages/HomePage/HomePage";
-import SharedLayout from "./components/SharedLayout/SharedLayout";
-import ErrorPage from "./pages/ErrorPage/ErrorPage";
+// import WelcomePage from "./pages/WelcomePage/WelcomePage";
+// import HomePage from "./pages/HomePage/HomePage";
+// import ErrorPage from "./pages/ErrorPage/ErrorPage";
+// import SharedLayout from "./components/SharedLayout/SharedLayout";
+// import AuthPage from "./pages/AuthPage/AuthPage";
+// import RegisterPage from "./pages/AuthPage/AuthFormPage/RegisterPage";
+// import LoginPage from "./pages/AuthPage/AuthFormPage/LoginPage";
 
-const test = import.meta.env.VITE_API_TEST;
+const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const SharedLayout = lazy(
+  () => import("./components/SharedLayout/SharedLayout")
+);
+const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage"));
+const AuthPage = lazy(() => import("./pages/AuthPage/AuthPage"));
+const LoginPage = lazy(() => import("./pages/AuthPage/AuthFormPage/LoginPage"));
+const RegisterPage = lazy(
+  () => import("./pages/AuthPage/AuthFormPage/RegisterPage")
+);
 
 const App = () => {
   return (
@@ -13,12 +27,14 @@ const App = () => {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<WelcomePage />} />
 
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/auth/:id" element={<AuthPage />} />
+        <Route path="auth/login" element={<LoginPage />} />
+        <Route path="auth/register" element={<RegisterPage />} />
 
+        <Route path="/home" element={<HomePage />} />
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
 };
-
 export default App;
--- src/components/AuthForm/RegisterForm/RegisterForm.jsx
+++ src/components/AuthForm/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,57 @@
-const RegisterForm = () => {
+import { useForm } from "react-hook-form";
+import styles from "./Register.module.css";
+
+function RegisterForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data));
+    reset();
+  };
   return (
     <div>
-      <h2>Register</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label className={styles.block}>
+          Enter your name:
+          <input
+            {...register("firstName", {
+              required: "Required field",
+            })}
+          />
+        </label>
+        <label className={styles.block}>
+          Enter your email:
+          <input type="email" {...register("email", { required: true })} />
+        </label>
+        <label className={styles.block}>
+          Create a password:
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 5,
+                message: "min 5 characters",
+              },
+            })}
+          />
+        </label>
+        <div style={{ height: 40 }}>
+          {errors?.firstName && <p>{errors?.firstName?.message || "Error!"}</p>}
+          {errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
+          {errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
+        </div>
+        <button type="submit">Register Now</button>
+      </form>
     </div>
   );
-};
+}
 
 export default RegisterForm;
