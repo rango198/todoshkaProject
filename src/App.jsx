32e0aed5e8a3055dd74ef3a9d00509caded8efc1
rango198 <137562811+rango198@users.noreply.github.com>
2024-05-06 08:47:09 +0300

    delete_pages_register_login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-
 import { lazy } from "react";
 import Modal from "./components/Modal/Modal";
 import { setModalContent, setModalStatus } from "./redux/slice/servicesSlice";
@@ -10,19 +9,13 @@ import SharedLayout from "./components/SharedLayout/SharedLayout";
 
 const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage"));
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-
 const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage"));
 const AuthPage = lazy(() => import("./pages/AuthPage/AuthPage"));
-const LoginPage = lazy(() => import("./pages/AuthPage/AuthFormPage/LoginPage"));
-const RegisterPage = lazy(
-  () => import("./pages/AuthPage/AuthFormPage/RegisterPage")
-);
 const ScreensPage = lazy(() => import("./pages/ScreensPage/ScreensPage"));
 
 const App = () => {
   const modalStatus = useSelector(selectOpenModal);
   const dispatch = useDispatch();
-
   const handleCloseModal = () => {
     dispatch(setModalStatus(false));
     dispatch(setModalContent({ action: null, recordDataEdit: null }));
@@ -32,20 +25,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<WelcomePage />} />
-
-          <Route path="/auth/:id" element={<AuthPage />}>
-            <Route path="auth/login" element={<LoginPage />} />
-            <Route path="auth/register" element={<RegisterPage />} />
-          </Route>
-
+          <Route path="/auth/:id" element={<AuthPage />} />
           <Route path="/home" element={<HomePage />}>
             <Route path=":boardName" element={<ScreensPage />} />
           </Route>
-
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
-
       <Modal open={modalStatus} onClose={handleCloseModal}>
         {<ModalContent />}
       </Modal>
