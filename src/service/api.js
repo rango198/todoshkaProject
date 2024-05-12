15a2cc4637071fa9e8e90c8de901eaf637f5ea69
rango198 <137562811+rango198@users.noreply.github.com>
2024-05-12 21:53:06 +0300

    api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://todoshka-back-5xf7.onrender.com/api/";
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "https://todoshka-back-5xf7.onrender.com/api/";
+// const BASE_URL = "http://localhost:3000/api/";
 const $instance = axios.create({ baseURL: BASE_URL });
 
 export const setAccessToken = (token) => {
