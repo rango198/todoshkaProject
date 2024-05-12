b48c86d4f963ce3386b6702fce05762017357b42
rango198 <137562811+rango198@users.noreply.github.com>
2024-05-12 17:38:29 +0300

    fixed_beackgraund

--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from "react";
-
+import { useSelector } from "react-redux";
 import Board from "../../components/Screens/Board/Board";
 import Icon from "../../components/Icon/Icon";
-
 import css from "./ScreenPage.module.css";
-
 import NewBoard from "../../components/Screens/NewBoard/NewBoard";
 import Modal from "../../components/Modal/Modal";
 import Filter from "../../components/Filter/Filter";
-
 import {
   selectAllBoards,
   selectedBoard,
 } from "../../redux/selectors/serviceSelector";
-import { useSelector } from "react-redux";
 
 const ScreensPage = () => {
   const [openFilter, setOpenFilter] = useState(false);
   const isBoards = useSelector(selectAllBoards);
-  const [backgroundImage, setBackgroundImage] = useState("");
+  const [color, setColor] = useState(null);
+  const { title, background } = useSelector(selectedBoard);
 
-  const { title, columns, background } = useSelector(selectedBoard);
-  const bgImage = background;
+  useEffect(() => {
+    reloadColor(background);
+  }, [background]);
 
-  // useEffect(() => {
-  //   if (bgImage) {
-  //     setBackgroundImage(`url("./assets/img/desk/${bgImage}-desk-2x.webp")`);
-  //   }
-  // }, [bgImage]);
+  const reloadColor = (background) => {
+    if (background === "default") {
+      setColor(null);
+    } else {
+      setColor(background);
+    }
+  };
 
   const handleOpenFilter = () => {
     setOpenFilter(true);
@@ -38,14 +38,13 @@ const ScreensPage = () => {
   };
 
   return (
-    <div className={`background-${bgImage}`}>
+    <div className={color ? `background-${color}` : "background-color-body"}>
       <div className={css.screen}>
         {openFilter && (
           <Modal open={handleOpenFilter} onClose={toggleFilter}>
             <Filter onClose={toggleFilter} />
           </Modal>
         )}
-
         <div className={css.title_container}>
           <span className={css.title_wrap}>
             <p className={css.title_board}>{title}</p>
