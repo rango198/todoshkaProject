6cc0ffe92c5deb4b96175ecf28ce2ce5b5dcaaf4
MariiaSam <kontragent@ukr.net>
2024-05-15 16:02:42 +0300

    add

--- src/components/ButtonAdd/ButtonAdd.jsx
+++ src/components/ButtonAdd/ButtonAdd.jsx
@@ -1,11 +1,11 @@
 import Icon from "../Icon/Icon.jsx";
 import css from "./buttonAdd.module.css";
 
-const ButtonAdd = ({ title, className, onClick }) => {
+const ButtonAdd = ({ title, className, onClick, iconStyle, iconSpan }) => {
   return (
     <button onClick={onClick} className={className}>
-      <span className={css.button_icon_bg}>
-        <Icon id="plus" className={css.button_icon} />
+      <span className={`${css.button_icon_bg} ${iconSpan}`}>
+        <Icon id="plus" className={`${css.button_icon} ${iconStyle}`} />
       </span>
       <span className={css.button_title}>{title}</span>
     </button>
